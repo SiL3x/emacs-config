3d1a5cf422cfb2fc54403b22447c6cff06d61f14
Sil3x <raven2020@gmx.de>
2022-05-11 13:28:59 +0200

    update packages and reinstall helm

---
+++ elpa/grip-mode-2.3.0/grip-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from grip-mode.el  -*- no-byte-compile: t -*-
+(define-package "grip-mode" "2.3.0" "Instant GitHub-flavored Markdown/Org preview using grip." '((emacs "24.4")) :commit "0ef2a3566f76e1c03ec64ac64cbb916530e40e32" :authors '(("Vincent Zhang" . "seagle0128@gmail.com")) :maintainer '("Vincent Zhang" . "seagle0128@gmail.com") :keywords '("convenience" "markdown" "preview") :url "https://github.com/seagle0128/grip-mode")
--- init.el
+++ init.el
@@ -22,10 +22,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wheatgrass)))
+ '(custom-enabled-themes '(wheatgrass))
  '(package-selected-packages
-   (quote
-    (grip-mode use-package markdown-mode ag multiple-cursors dockerfile-mode web-beautify json-mode rainbow-delimiters slime-volleyball flycheck-gometalinter helm company-go ivy magit projectile go-mode php-mode company-terraform terraform-doc terraform-mode))))
+   '(grip-mode use-package markdown-mode ag multiple-cursors dockerfile-mode web-beautify json-mode rainbow-delimiters slime-volleyball flycheck-gometalinter company-go ivy magit projectile go-mode php-mode company-terraform terraform-doc terraform-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
