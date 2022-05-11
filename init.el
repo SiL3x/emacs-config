5c6bb1a5e3295d8d9c47be4e1217f5d89777fec9
Sil3x <raven2020@gmx.de>
2022-05-11 12:42:21 +0200

    this broke everything

--- init.el
+++ init.el
@@ -22,10 +22,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wheatgrass)))
+ '(custom-enabled-themes '(wheatgrass))
  '(package-selected-packages
-   (quote
-    (grip-mode use-package markdown-mode ag multiple-cursors dockerfile-mode web-beautify json-mode rainbow-delimiters slime-volleyball flycheck-gometalinter helm company-go ivy magit projectile go-mode php-mode company-terraform terraform-doc terraform-mode))))
+   '(yaml-mode lsp-mode csv-mode grip-mode use-package markdown-mode ag multiple-cursors dockerfile-mode web-beautify json-mode rainbow-delimiters slime-volleyball flycheck-gometalinter helm company-go ivy magit projectile go-mode php-mode company-terraform terraform-doc terraform-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -40,6 +39,33 @@
   :config
   (progn
     (flycheck-gometalinter-setup)))
+;; Set different deadline (default: 5s)
+(setq flycheck-gometalinter-deadline "10s")
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (go-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; (require 'lsp-mode) should be okay
+(add-hook 'go-mode-hook #'lsp-deferred)
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; better use gopls
+;; (require 'company)                                   ; load company mode
+;; (require 'company-go)                                ; load company mode go backend
+
 
 
 ;; Ivy config
